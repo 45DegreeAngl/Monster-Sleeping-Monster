fffe2271ebc3ae88c882fbdf7ae7cb7e43b0cbab
KingCorvid <91034705+KingCorvid@users.noreply.github.com>
2025-02-16 21:58:05 -0500

    blood

--- Hittables/pedestrian.gd
+++ Hittables/pedestrian.gd
@@ -4,6 +4,7 @@ signal ive_been_hit(points,color)
 
 
 var destroyed : bool = false
+@export var bloodSpray:CPUParticles3D
 @export var point_value = 100
 @export var mesh : Node3D
 
@@ -11,6 +12,7 @@ func collateral(hit_velocity):
 	self.apply_central_force(hit_velocity * 100)
 	$AudioStreamPlayer3D.volume_db = pow(hit_velocity.length(),1.0/4.0)
 	$AudioStreamPlayer3D.play()
+	bloodSpray.emitting = true
 	if !destroyed:
 		ive_been_hit.emit(point_value,$Mesh.mesh.surface_get_material(0).albedo_color)
 		destroyed = true
